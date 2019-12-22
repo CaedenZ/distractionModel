3ff1f81cbdcb662671bb49b8c4b1188d66307e13
Jared Quek <e@Jareds-Mac-mini.local>
2019-12-22 10:52:10 +0800

    Updated

--- run_local_cv.py
+++ run_local_cv.py
@@ -1,17 +1,28 @@
-from analysis_rt import analysis
+# Run this file on CV2 in local machine to construct a Concentration Index (CI).
+# Video image will show emotion on first line, and engagement on second. Engagement/concentration classification displays either 'Pay attention', 'You are engaged' and 'you are highly engaged' based on CI. Webcam is required.
+# Analysis is in 'Util' folder.
+
+
+from util.analysis_realtime import analysis
 import cv2
 import numpy as np
 
+# Initializing
 cap = cv2.VideoCapture(0)
 ana = analysis()
 
+# Capture every frame and send to detector
 while True:
     _, frame = cap.read()
     bm = ana.detect_face(frame)
+
+    cv2.imshow("Frame", frame)
+
     key = cv2.waitKey(1)
+# Exit if 'q' is pressed
     if key == ord('q'):
         break
 
-
+# Release the memory
 cap.release()
 cv2.destroyAllWindows()
