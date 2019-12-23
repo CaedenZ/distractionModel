792b7d427ee68dee207a7617032f91dcdd99a713
CaedenZ <FENGYE.16@ichat.sp.edu.sg>
2019-12-23 10:43:58 +0800

    Some configure of code

--- run_server.py
+++ run_server.py
@@ -55,27 +55,12 @@ def data_uri_to_cv2_img(uri):
 @cross_origin(origin='*', headers=['Content-Type'])
 def predictImage():
     data = request.get_json(force=True)
-    # print(data)
     datainString = data.get('image')
-    # decoded_data = base64.b64decode(datainString)
-    # np_data = np.fromstring(decoded_data,np.uint8)
     img = readb64(datainString)
     cv2.imshow("test", img)
-    # print(datainString)
-    # img = data_uri_to_cv2_img(data_uri)
-    # cv2.imshow(img)
-    # image_data = np.asarray(imgdata)
-    # print(datainString)
-    # print(camData.get('camera_ip_address'))
-    # IP = data.get('ipAdd')
-    ######## activate with scheduler ########
-    # cScheduler = cam_scheduler(cam, schedule_list)
-    # cScheduler.start()
-    # threadDict[IP] = cScheduler
-    # returnData = ana.detect_face(source)
+    returnData = ana.detect_face(img)
 
-    # cv2.imshow('Frame',source)
-    response = make_response('returnData')
+    response = make_response(returnData)
     response.headers.set('Content-Type', 'application/json')
     return response
 
