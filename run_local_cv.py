2604afe19527c513ceb4d58fe042a823c417ae64
MohammedAljahdali <mohammedkhalid4028@gmail.com>
2023-03-01 23:04:12 +0300

    Add video storing

--- run_local_cv.py
+++ run_local_cv.py
@@ -7,6 +7,12 @@ from util.analysis_realtime import analysis
 import cv2
 import numpy as np
 
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('recorded_video.avi', fourcc, 20.0, (640,  480))
+out_with_result = cv2.VideoWriter('recorded_video_with_result.avi', fourcc, 20.0, (640,  480))
+
 # Initializing
 cap = cv2.VideoCapture(0)
 ana = analysis()
@@ -14,10 +20,11 @@ ana = analysis()
 # Capture every frame and send to detector
 while True:
     _, frame = cap.read()
+    out.write(frame)
     bm = ana.detect_face(frame)
 
     cv2.imshow("Frame", frame)
-
+    out_with_result.write(frame)
     key = cv2.waitKey(1)
 # Exit if 'q' is pressed
     if key == ord('q'):
@@ -25,4 +32,6 @@ while True:
 
 # Release the memory
 cap.release()
+out.release()
+out_with_result.release()
 cv2.destroyAllWindows()
